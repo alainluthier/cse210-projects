e8b833d4d3302abc30deb52a086a3d9905243932
alainluthier <alainluthier@gmail.com>
2023-09-18 16:05:19 -0400

    programWeek02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2018;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Project Manager";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        /*Console.WriteLine(job1._company);
+        Console.WriteLine(job2._company);*/
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Console.WriteLine(myResume._jobs[0]._jobTitle);
+
+        myResume.Display();
     }
 }
