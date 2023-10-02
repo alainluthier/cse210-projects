74429b5685439746ea03c487df5d94b1e45d5f80
alainluthier <alainluthier@gmail.com>
2023-10-02 15:23:13 -0400

    learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment = new Assignment("Alain Ramos","Stadistics");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Miranda","Multiplications","1.1","8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Eithan Hansen","History","The History of Bolivia by Carlos Mesa");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
